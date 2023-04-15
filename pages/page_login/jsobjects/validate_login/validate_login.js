aa2019c9ea098e5a2eb5aef623dd932dc7158453
Sriram Manikanth <msriram0803@gmail.com>
2023-04-15 17:59:04 +0000

    System generated commit, initial commit

---
+++ pages/page_login/jsobjects/validate_login/validate_login.js
@@ -0,0 +1,9 @@
+export default {
+	validate_login () {
+		var db_passwd = validateuser.data;
+		console.log(db_passwd)
+		if (db_passwd["values"][0] == passwd.inputText){
+			return true 
+		}
+	}
+}
